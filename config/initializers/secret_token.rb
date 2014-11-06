591199287e2360aaa11e06481ec615471edeafcc
Robert Hedwall <pang0@countermail.com>
2014-11-06 10:32:32 -0500

    Initial Commit
    New project files - RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amazinganaconda::Application.config.secret_key_base = '7423a6f3523d77c8530d91dc19896139ef71328bde0cc1685a24af2d14a906122819010bcfdc9b75abb35a81a3aba2b3a40d6f8a3be58b6ac5aeb8b7aac01357'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Amazinganaconda::Application.config.session_store :cookie_store, key: '_amazinganaconda_session'
